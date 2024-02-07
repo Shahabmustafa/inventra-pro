72d6904a659275580c016581e24390b210240de0
Shahabmustafa <shahabmustafa57@gmail.com>
2024-02-07 15:01:39 +0500

    add code

--- lib/screens/home/admin/search_vin_screen.dart
+++ lib/screens/home/admin/search_vin_screen.dart
@@ -2,6 +2,7 @@ import 'package:InventaPro/screens/home/admin/update_vehical_seller_information_
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import '../vehicale saller information/vehical_detail_screen_two.dart';
 
 class SearchVinScreen extends StatefulWidget {
@@ -82,82 +83,49 @@ class _SearchVinScreenState extends State<SearchVinScreen> {
                       itemBuilder: (context,index){
                         return Card(
                           // key: snapshot.data!.docs[index].id,
-                          color: Colors.white,
-                          child: ListTile(
-                            leading: Container(
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(100),
-                                child: CachedNetworkImage(
-                                  height: 50,
-                                  width: 50,
-                                  fit: BoxFit.cover,
-                                  imageUrl: snapshot.data!.docs[index]["image6"],
-                                  placeholder: (context, url) => CircularProgressIndicator(),
-                                  errorWidget: (context, url, error) => Icon(Icons.error),
+                            color: Colors.white,
+                            child: CardBox(
+                              title: snapshot.data!.docs[index]["make"],
+                              imageUrl: snapshot.data!.docs[index]["image1"],
+                              subTitile: snapshot.data!.docs[index]["vinn"],
+                              tapDelete: (){
+                                FirebaseFirestore.instance
+                                    .collection("vehicleandSalllerInformaation")
+                                    .doc(snapshot.data!.docs[index].id).delete();
+                              },
+                              tapEdit: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => UpdateVehicalSellerInformation(
+                                  uid: snapshot.data!.docs[index].id,
+                                  sellerName: snapshot.data?.docs[index]["name_of_seller"],
                                 ),
-                              ),
-                            ),
-                            title: Text(snapshot.data!.docs[index]["make"]),
-                            subtitle: Text(snapshot.data!.docs[index]["vinn"]),
-                            trailing: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                InkWell(
-                                  onTap: (){
-                                    Navigator.push(context, MaterialPageRoute(builder: (context) => UpdateVehicalSellerInformation(
-                                      uid: snapshot.data!.docs[index].id,
-                                      sellerName: snapshot.data?.docs[index]["name_of_seller"],
-                                    ),
-                                    ),
-                                    );
-                                  },
-                                  child: Icon(
-                                    Icons.edit,
-                                    color: Colors.blue,
-                                  ),
                                 ),
-                                SizedBox(
-                                  height: 8,
+                                );
+                              },
+                              pressCard: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => VehicleDetailScreenTwo(
+                                  image1: snapshot.data!.docs[index]["image1"],
+                                  image2: snapshot.data!.docs[index]["image2"],
+                                  image3: snapshot.data!.docs[index]["image3"],
+                                  image4: snapshot.data!.docs[index]["image4"],
+                                  image5: snapshot.data!.docs[index]["image5"],
+                                  image6: snapshot.data!.docs[index]["image6"],
+                                  image7: snapshot.data!.docs[index]["image7"],
+                                  image8: snapshot.data!.docs[index]["image8"],
+                                  image9: snapshot.data!.docs[index]["image9"],
+                                  image10: snapshot.data!.docs[index]["image10"],
+                                  image11: snapshot.data!.docs[index]["image11"],
+                                  date: snapshot.data!.docs[index]["date"],
+                                  address: snapshot.data!.docs[index]["address"],
+                                  model: snapshot.data!.docs[index]["model"],
+                                  make: snapshot.data!.docs[index]["make"],
+                                  name_of_seller: snapshot.data!.docs[index]["name_of_seller"],
+                                  vinn: snapshot.data!.docs[index]["vinn"],
+                                  year: snapshot.data!.docs[index]["year"],
+                                  purchase_Price: snapshot.data!.docs[index]["purchase_Price"],
                                 ),
-                                InkWell(
-                                  onTap: (){
-                                    FirebaseFirestore.instance
-                                        .collection("vehicleandSalllerInformaation")
-                                        .doc(snapshot.data!.docs[index].id).delete();
-                                  },
-                                  child: Icon(
-                                    Icons.delete,
-                                    color: Colors.red,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            onTap: (){
-                              Navigator.push(context, MaterialPageRoute(builder: (context) => VehicleDetailScreenTwo(
-                                image1: snapshot.data!.docs[index]["image1"],
-                                image2: snapshot.data!.docs[index]["image2"],
-                                image3: snapshot.data!.docs[index]["image3"],
-                                image4: snapshot.data!.docs[index]["image4"],
-                                image5: snapshot.data!.docs[index]["image5"],
-                                image6: snapshot.data!.docs[index]["image6"],
-                                image7: snapshot.data!.docs[index]["image7"],
-                                image8: snapshot.data!.docs[index]["image8"],
-                                image9: snapshot.data!.docs[index]["image9"],
-                                image10: snapshot.data!.docs[index]["image10"],
-                                image11: snapshot.data!.docs[index]["image11"],
-                                date: snapshot.data!.docs[index]["date"],
-                                address: snapshot.data!.docs[index]["address"],
-                                model: snapshot.data!.docs[index]["model"],
-                                make: snapshot.data!.docs[index]["make"],
-                                name_of_seller: snapshot.data!.docs[index]["name_of_seller"],
-                                vinn: snapshot.data!.docs[index]["vinn"],
-                                year: snapshot.data!.docs[index]["year"],
-                                purchase_Price: snapshot.data!.docs[index]["purchase_Price"],
-                              ),
-                              ),
-                              );
-                            },
-                          ),
+                                ));
+                              },
+                            )
                         );
                       },
                     ) :
@@ -168,56 +136,24 @@ class _SearchVinScreenState extends State<SearchVinScreen> {
                         return Card(
                           // key: snapshot.data!.docs[index].id,
                           color: Colors.white,
-                          child: ListTile(
-                            leading: Container(
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(100),
-                                child: CachedNetworkImage(
-                                  height: 50,
-                                  width: 50,
-                                  fit: BoxFit.cover,
-                                  imageUrl: snapshot.data!.docs[index]["image6"],
-                                  placeholder: (context, url) => CircularProgressIndicator(),
-                                  errorWidget: (context, url, error) => Icon(Icons.error),
-                                ),
+                          child: CardBox(
+                            title: snapshot.data!.docs[index]["make"],
+                            imageUrl: snapshot.data!.docs[index]["image1"],
+                            subTitile: snapshot.data!.docs[index]["vinn"],
+                            tapDelete: (){
+                              FirebaseFirestore.instance
+                                  .collection("vehicleandSalllerInformaation")
+                                  .doc(snapshot.data!.docs[index].id).delete();
+                            },
+                            tapEdit: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => UpdateVehicalSellerInformation(
+                                uid: snapshot.data!.docs[index].id,
+                                sellerName: snapshot.data?.docs[index]["name_of_seller"],
                               ),
-                            ),
-                            title: Text(snapshot.data!.docs[index]["make"]),
-                            subtitle: Text(snapshot.data!.docs[index]["vinn"]),
-                            trailing: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                InkWell(
-                                  onTap: (){
-                                    Navigator.push(context, MaterialPageRoute(builder: (context) => UpdateVehicalSellerInformation(
-                                      uid: snapshot.data!.docs[index].id,
-                                      sellerName: snapshot.data?.docs[index]["name_of_seller"],
-                                    ),
-                                    ),
-                                    );
-                                  },
-                                  child: Icon(
-                                    Icons.edit,
-                                    color: Colors.blue,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 5,
-                                ),
-                                InkWell(
-                                  onTap: (){
-                                    FirebaseFirestore.instance
-                                        .collection("vehicleandSalllerInformaation")
-                                        .doc(snapshot.data!.docs[index].id).delete();
-                                  },
-                                  child: Icon(
-                                    Icons.delete,
-                                    color: Colors.red,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            onTap: (){
+                              ),
+                              );
+                            },
+                            pressCard: (){
                               Navigator.push(context, MaterialPageRoute(builder: (context) => VehicleDetailScreenTwo(
                                 image1: snapshot.data!.docs[index]["image1"],
                                 image2: snapshot.data!.docs[index]["image2"],
@@ -239,10 +175,9 @@ class _SearchVinScreenState extends State<SearchVinScreen> {
                                 year: snapshot.data!.docs[index]["year"],
                                 purchase_Price: snapshot.data!.docs[index]["purchase_Price"],
                               ),
-                              ),
-                              );
+                              ));
                             },
-                          ),
+                          )
                         );
                       },
                     ) : Text("");
@@ -258,3 +193,79 @@ class _SearchVinScreenState extends State<SearchVinScreen> {
     );
   }
 }
+
+
+
+class CardBox extends StatelessWidget {
+  CardBox({required this.title,required this.imageUrl,required this.subTitile,required this.tapDelete,required this.pressCard,required this.tapEdit,super.key});
+  String imageUrl;
+  String title;
+  String subTitile;
+  VoidCallback pressCard;
+  VoidCallback tapEdit;
+  VoidCallback tapDelete;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: pressCard,
+      child: Container(
+        height: 70,
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          // color: Colors.grey.shade200,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: tapEdit,
+                  child: Icon(Icons.edit),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: CachedNetworkImage(
+                    height: 50,
+                    width: 50,
+                    fit: BoxFit.cover,
+                    imageUrl: imageUrl,
+                    placeholder: (context, url) => CircularProgressIndicator(),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(subTitile,style: TextStyle(color: Colors.black),),
+                  ],
+                ),
+              ],
+            ),
+            GestureDetector(
+              onTap: tapDelete,
+              child: Icon(Icons.delete,color: Colors.red,),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
